6e3098d067b1210f0f850edc7b0a2f1565b14d5c
Roy Bar-On <royb997@gmail.com>
2022-01-27 15:59:06 +0200

    Assembly code
    wrote the asm code for the simulation, should work fine.

--- test.s
+++ test.s
@@ -1,16 +1,19 @@
 .text
 main:   # Start of your code
 
-        # End of your code
-        add     t6, x0, x0
-        beq     t6, x0, finish
+    lw      t1, 0x08(x0)
+    lw      t2, 0x0c(x0)
+    sw2     t1, 0x10(x0)
+    sw2     t2, 0x14(x0)
+
+    # End of your code
+    add     t6, x0, x0
+    beq     t6, x0, finish
 
 deadend: beq    t6, x0, deadend
 
 finish:
-        lw      t4, 0(x0)
-        lw      t5, 4(x0)
-        sw      t5, 0xFF(t4)
-        beq     t6, x0, deadend
-
-
+    lw      t4, 0(x0)
+    lw      t5, 4(x0)
+    sw      t5, 0xFF(t4)
+    beq     t6, x0, deadend
